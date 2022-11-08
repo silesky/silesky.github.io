44dde30e5b163974bf8be984faf32fc3d0cad209
Seth Silesky <5115498+silesky@users.noreply.github.com>
2022-11-08 11:52:46 -0600

    move to sep script

---
+++ ajs.js
@@ -0,0 +1,52 @@
+const createEvent = (eventName, cb, selector) => {
+  document.querySelector(selector).addEventListener("click", function (e) {
+    e.preventDefault();
+    var contents = document.querySelector("#event").value;
+    var evt = JSON.parse(contents);
+    console.profile(eventName);
+    console.time(eventName);
+    var promise = cb(evt);
+    promise.then(function (ctx) {
+      console.timeEnd(eventName);
+      console.profileEnd(eventName);
+      ctx.flush();
+      document.querySelector("#logs").textContent = JSON.stringify(
+        ctx.event,
+        null,
+        "  "
+      );
+    });
+  });
+};
+
+createEvent(
+  "track",
+  (evt) => {
+    return window.analytics.track(
+      evt.name || "",
+      evt.properties || {},
+      evt.options || {}
+    );
+  },
+  "#track"
+);
+
+createEvent(
+  "identify",
+  (evt) => {
+    return window.analytics.identify(
+      evt.name || "",
+      evt.properties || {},
+      evt.options || {}
+    );
+  },
+  "#identify"
+);
+
+createEvent(
+  "page",
+  (evt) => {
+    return window.analytics.page(evt.properties);
+  },
+  "#page"
+);
